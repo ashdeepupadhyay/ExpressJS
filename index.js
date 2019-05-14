94940a6489ba5bcfbebb3833c59f6d840b8f905a
ashdeepupadhyay <ashdeepupadhyay@gmail.com>
2019-05-14 19:31:56 +0530

    logging per hit on api with date and time

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
 const express=require('express');
 const path =require('path');
 const members=require('./Members');
-
+const logger=require('./middleware/logger');
 const app=express();
 
-//creating a middle ware
-const logger=(req,res,next)=>{
-    console.log(`${req.protocol}://${req.get('host')}${req.originalUrl}`);
-    next();
-}
+
 
 //init middleware
 app.use(logger);
