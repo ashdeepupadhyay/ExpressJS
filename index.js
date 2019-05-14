0928d8b93569d85a694bd2212e02e52c4dda1c20
ashdeepupadhyay <ashdeepupadhyay@gmail.com>
2019-05-14 19:45:47 +0530

    getting a single member through a api

--- index.js
+++ index.js
@@ -7,13 +7,24 @@ const app=express();
 
 
 //init middleware
-app.use(logger);
+//app.use(logger);
 
 //Gets All Members
 app.get('/api/members',(req,res)=>{
     res.json(members);
 });
 
+//Get Single Member
+app.get('/api/members/:id',(req,res)=>{
+    const found=members.some(members=>members.id===parseInt(req.params.id));
+
+    if(found){
+        res.json(members.filter(members=>members.id===parseInt(req.params.id)));
+    }
+    else{
+        res.status(400).json({msg:`No member with the id of ${req.params.id}`});
+    }
+})
 
 //to route
 // app.get('/',(req,res)=>{
