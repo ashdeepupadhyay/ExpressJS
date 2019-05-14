1e6f6a2b097958adc12be284ab22fed312f88bbc
ashdeepupadhyay <ashdeepupadhyay@gmail.com>
2019-05-14 19:02:32 +0530

    creating a static server

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const express=require('express');
+const path =require('path');
 
 const app=express();
+
+//to route
+// app.get('/',(req,res)=>{
+//     //res.send(`<h1>Hello World!!</h1>`);
+//     res.sendFile(path.join(__dirname,'public','index.html'));
+// });
+
+//set static folder
+app.use(express.static(path.join(__dirname,'public')));
 
 const PORT=process.env.PORT|5000;//when we deploy server will not run on 5000 
 
